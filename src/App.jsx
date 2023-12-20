b1d23fae5d2eb40d70c99fc727bf80c75a8fe30f
Shamal Muneer <shamal20017@gmail.com>
2023-12-20 22:02:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
